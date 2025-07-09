03ce2f1be234f344e0570f249b1488b138992db2
Algeron1 <61060622+Algeron1@users.noreply.github.com>
2025-07-09 21:13:43 +0700

    add variables

--- src/test/java/com/rgs/bamboonotifier/RgsBambooNotifierApplicationTests.java
+++ src/test/java/com/rgs/bamboonotifier/RgsBambooNotifierApplicationTests.java
@@ -2,8 +2,10 @@ package com.rgs.bamboonotifier;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RgsBambooNotifierApplicationTests {
 
     @Test
